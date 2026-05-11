0ed96f8c39ce8dcd93f183b05b163ea3398b0219
dbtfreeaccount.negotiate461@passmail.net <dbtfreeaccount.negotiate461@passmail.net>
2026-05-11 17:48:37 +0000

    correction of the data to match year 2018

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -7,7 +7,7 @@ DATE(TO_TIMESTAMP(STARTED_AT)) AS DATE_STARTED_AT,
 HOUR(TO_TIMESTAMP(STARTED_AT)) AS HOUR_STARTED_AT,
 {{get_day_type('STARTED_AT')}} AS DAY_TYPE,
 {{get_season('STARTED_AT')}} AS STATION_OF_YEAR
-from {{ source('demo', 'bike') }}
+from {{ ref('stg_bike') }}
 where started_at != 'started_at'
 )
 select *
